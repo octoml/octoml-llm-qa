d6033b96b4129673a7f81ea79c50a669e1b4ec9e
Ian Sawicki <iansawicki@Ians-MacBook-Pro-2.local>
2023-06-07 05:29:17 -0700

    setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='octollms',
+    name='octo_llm',
     version='1.0.0',
     author='',
     author_email='',
     description='Python utils for langchain',
-    packages=find_packages(),
+    packages=['octo_llm'],
     install_requires=[
         'langchain',
         'llama_index',
